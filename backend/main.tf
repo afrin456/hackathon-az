d9014d110a2982ed321636c7a477d21c9ca76300
Afrin M A <afrinafi456@gmail.com>
2025-11-08 14:53:17 +0530

    updated aws terraform resources

--- backend/main.tf
+++ backend/main.tf
@@ -4,13 +4,16 @@ provider "aws" {
 
 resource "aws_s3_bucket" "tf_state" {
   bucket = "tf-state-bucket"
-  versioning {
-    enabled = true
-  }
+
   lifecycle {
     prevent_destroy = true
   }
 }
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 
 # resource "aws_dynamodb_table" "tf_locks" {
 #   name         = "terraform-locks"
