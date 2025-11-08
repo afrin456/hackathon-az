b2f4af467123b367edd5807095b2070f35ae731c
Afrin M A <afrinafi456@gmail.com>
2025-11-08 17:51:51 +0530

    updated resources

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,3 +2,6 @@ resource "aws_ecr_repository" "patient" {
   name = "${var.env}-patient-repo"
 }
 
+resource "aws_ecr_repository" "application" {
+  name = "${var.env}-patient"
+}
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   azs             = var.availability_zones
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
-
+  map_public_ip_on_launch = true
   enable_nat_gateway = true
   single_nat_gateway = true
   enable_dns_hostnames = true
