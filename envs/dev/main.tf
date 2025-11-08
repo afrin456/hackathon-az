b73fdca0d3a8729aee5c2caf8147780faabcb906
Afrin M A <afrinafi456@gmail.com>
2025-11-08 15:14:57 +0530

    updated aws terraform resources

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-bucket"
+    bucket         = "tf-state-bucket-ms-999"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
