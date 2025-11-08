b0aa0b2a6044c60f84e0a3d46b32aee66d9d775f
Afrin M A <afrinafi456@gmail.com>
2025-11-08 16:02:19 +0530

    updated aws terraform resources

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,8 +2,19 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "${var.env}-eks-js-ms"
+  name               = "my-cluster"
   kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
 
   # Optional
   endpoint_public_access = true
@@ -11,14 +22,22 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  # Create just the IAM resources for EKS Auto Mode for use with custom node pools
-  create_auto_mode_iam_resources = true
-  compute_config = {
-    enabled = true
-  }
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.public_subnets
+  control_plane_subnet_ids = var.public_subnets
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.public_subnets
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      #ami_type       = "ami-0c55b159cbfafe1f0"
+      instance_types = ["t2.micro"]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+    }
+  }
 
   tags = {
     Environment = "dev"
